2e9f7edb4f94edaac1d041ceda5e365a37ac6d85
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-27 11:29:27 +0530

    initialized prisma and added snippet page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Button >click</Button>
+      <h1 className="font-bold text-xl">Home</h1>
+      <div className="flex items-center justify-between">
+        <h1>Snippets</h1>
+        <Link href={"/snippet/new"} ><Button>New</Button></Link>
+      </div>
     </div>
   );
 }
---
+++ src/app/snippet/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewSnippetPage = () => {
+  return (
+    <div>
+      new snippet page
+    </div>
+  )
+}
+
+export default NewSnippetPage
