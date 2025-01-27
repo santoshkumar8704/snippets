da80f2a2d04bb2d28590933c3824612a5a2dfcb3
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-27 11:37:17 +0530

    designed add snippet page

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { Input } from '@/components/ui/input'
 const NewSnippetPage = () => {
   return (
-    <div>
-      new snippet page
-    </div>
+    <form>
+      <div>
+        <Label>Title</Label>
+        <Input type='text' id='title' name='title' />
+      </div>
+      <div>
+        <Label>Code</Label>
+        <Textarea id='code' name='code' />
+      </div>
+      <Button>Add</Button>
+    </form>
   )
 }
 
