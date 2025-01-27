157ed47d991f634d8c7d81d638b151ead03ad699
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-27 12:35:07 +0530

    added functionality to add a snippet to a db

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -3,9 +3,29 @@ import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 import { Input } from '@/components/ui/input'
+import { prisma } from '@/lib/prisma'
+import { redirect } from 'next/navigation'
+
 const NewSnippetPage = () => {
+    const createSnippet = async (formData:FormData) => {
+        "use server"
+        const title = formData.get('title') as string
+        const code = formData.get('code') as string
+        const snippet = await prisma.snippet.create({
+          data:{
+            title,
+            code
+
+          }
+    
+        })
+        console.log(snippet)
+        redirect("/")
+
+
+    }
   return (
-    <form>
+    <form action={createSnippet}>
       <div>
         <Label>Title</Label>
         <Input type='text' id='title' name='title' />
